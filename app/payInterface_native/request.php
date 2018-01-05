5ffc6d343c291713aa83c2ed5e4c2e0d429f8901
zhenglunsen <1214422067@qq.com>
2018-01-05 13:11:22 +0800

    pay02

--- app/payInterface_native/request.php
+++ app/payInterface_native/request.php
@@ -336,7 +336,7 @@ Class Request_wechat{
                         if($status ==1)
                         {   
 
-                            Utils_wechat_wechat::dataRecodes('接口回调收到通知参数',$this->resHandler->getAllParameters());
+                            Utils_wechat::dataRecodes('接口回调收到通知参数',$this->resHandler->getAllParameters());
                             echo 'success';
                             file_put_contents('pay/wechat/2.txt',1);//如果生成2.txt,说明前一步的输出success是有执行
                             \DB::table('cs')->insert(['cs'=>$res->id.'-'.$create_id]);
