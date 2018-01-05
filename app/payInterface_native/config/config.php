201f355f130ed9ad508fcaaaa78a9c4d40fbbdb9
zhenglunsen <1214422067@qq.com>
2018-01-05 11:00:42 +0800

    pay02

--- app/payInterface_native/config/config.php
+++ app/payInterface_native/config/config.php
@@ -5,7 +5,7 @@ class Config_wechat{
         'mchId'=>'102585335014',//测试商户号，商户上线需改为自己正式的
         'key'=>'b8770c61ddf911e40393d5addb87f1a0',//测试密钥，商户上线需改为自己正式的
         'version'=>'2.0',
-		'notify_url'=>'www.jianshanglianmeng.com/payment/wechat/notify'//异步回调通知地址，商户上线需改为自己正式的
+		'notify_url'=>'http://www.jianshanglianmeng.com/payment/wechat/notify'//异步回调通知地址，商户上线需改为自己正式的
        );
     
     public function C($cfgName){
