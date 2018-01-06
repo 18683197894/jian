41e0e69608db28a350fc0b54179398e6d1df373f
zhenglunsen <1214422067@qq.com>
2018-01-06 11:40:47 +0800

    pay03

--- public/home/images/payment/payments/common.js
+++ public/home/images/payment/payments/common.js
@@ -1,50 +1,50 @@
 
-// $(function(){
-// 	var id = $('.payments_1ul2').attr('index');
-// 	 a = setInterval(function(){
+$(function(){
+	var id = $('.payments_1ul2').attr('index');
+	 a = setInterval(function(){
 
-// 		payget(id,a);
+		payget(id,a);
 
-// 	},3000);
+	},3000);
 	
-// })
+})
 	
-// 	function payget(id)
-// 	{
-// 		// alert(id);
-// 		$.ajax('/payments/pay/paymentsget',{
-// 			type : 'get',
-// 			data : {id:id},
-// 			success : function(data,a)
-// 			{
-// 				if(data == 4)
-// 				{
-// 					alert('订单异常！请重新下单');
-// 					clearInterval(a);
-// 					window.location.href='/home/shoppingcart'
-// 					return false;
-// 				}
-// 				if(data == 3)
-// 				{
-// 					alert('订单已失效！请重新下单');
-// 					clearInterval(a);
-// 					window.location.href='/home/shoppingcart'
-// 				}
-// 				if(data == 2)
-// 				{
-// 					console.log('false');
-// 				}
-// 				if(data == 1)
-// 				{
-// 					alert('付款成功！将自动跳转自首页');
-// 					clearInterval(a);
-// 					window.location.href='/home/shoppingcart'
-// 				}
-// 			},
-// 			error : function(data)
-// 			{
-// 				console.log('拉取失败！');
-// 			}
+	function payget(id)
+	{
+		// alert(id);
+		$.ajax('/payments/pay/paymentsget',{
+			type : 'get',
+			data : {id:id},
+			success : function(data,a)
+			{
+				if(data == 4)
+				{
+					alert('订单异常！请重新下单');
+					clearInterval(a);
+					window.location.href='/home/shoppingcart'
+					return false;
+				}
+				if(data == 3)
+				{
+					alert('订单已失效！请重新下单');
+					clearInterval(a);
+					window.location.href='/home/shoppingcart'
+				}
+				if(data == 2)
+				{
+					console.log('false');
+				}
+				if(data == 1)
+				{
+					alert('付款成功！将自动跳转自首页');
+					clearInterval(a);
+					window.location.href='/home/shoppingcart'
+				}
+			},
+			error : function(data)
+			{
+				console.log('拉取失败！');
+			}
 			
-// 		})
-// 	}
+		})
+	}
