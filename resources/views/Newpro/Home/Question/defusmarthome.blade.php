32b20d44098ec9e81c22a6532b6c808413bcb304
zhenglunsen <1214422067@qq.com>
2018-03-17 16:34:43 +0800

    newsimg

--- resources/views/Newpro/Home/Question/defusmarthome.blade.php
+++ resources/views/Newpro/Home/Question/defusmarthome.blade.php
@@ -11,7 +11,12 @@
 <body>
 <img src="{{ asset('/new/home/question/img/smartbj.jpg')}}" alt="" class="bg" />
 
-<div class="title">智能家居市场调研问答卷</div>
+<div class="title">
+智能家居市场调研问答卷
+<div class="Music">
+    <audio id="bgMusic" src="{{ asset('/new/home/question/img/huijia.mp3') }}" autoplay="autoplay" loop="loop"></audio>
+</div>
+</div>
 <div class="brief">品质生活 从这里开始</div>
 <div class="container-box">
     <div class="content box1 avtive">
@@ -266,9 +271,7 @@
     </div>
 </div>
 <div class="Ok_oder"></div>
-<div class="Music">
-    <audio id="bgMusic" src="{{ asset('/new/home/question/img/huijia.mp3') }}" autoplay="autoplay" loop="loop"></audio>
-</div>
+
 <script src="{{ asset('/js/jquery-1.8.3.min.js') }}"></script>
 <script src="{{ asset('/new/home/question/js/defusmarthome.js')}}"></script>
 <script>
