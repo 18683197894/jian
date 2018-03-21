fe0824b709c1d3f66ef67f4756283653d65154ce
zhenglunsen <1214422067@qq.com>
2018-03-21 14:41:56 +0800

    20180321

--- app/Http/Controllers/Pay/PayController.php
+++ app/Http/Controllers/Pay/PayController.php
@@ -42,7 +42,7 @@ class PayController extends Controller
 		{
 			
 				if($res->status == 1 && !empty($res->create_id))
-				{
+				{ 
 					return response()->json(1);
 				}else
 				{
@@ -61,11 +61,30 @@ class PayController extends Controller
     {
 
       $id = $request->id;
-      $res = \DB::table('orders_diy')->select('id','_token','status','create_id','addtime')->where('id',$id)->first();
+      $res = \DB::table('orders_diy')->select('id','_token','phone','status','name','create_id','addtime')->where('id',$id)->first();
       if($res)
       {
         if($res->status == 1 && !empty($res->create_id))
-        {
+        {   
+            $a = \DB::table('user_home')->select('id')->where('phone',$res->phone)->first();
+            if(!$a)
+            {
+              $data['time'] = time();
+              $data['uptime'] = $data['time'];
+              $data['phone'] = $res->phone;
+              $data['status'] = 1;
+              $data['name'] = $res->phone;
+              $data['names'] = $res->name;
+              $data['password'] = \Hash::make('123456');
+              $str="QWERTYUIOPASDFGHJKLZXCVBNM1234567890qwertyuiopasdfghjklzxcvbnm";
+              str_shuffle($str);
+              $token=substr(str_shuffle($str),26,15);
+              // dd($token);
+              $data['_token'] = \Hash::make($token);
+              \DB::table('user_home')->insert($data);
+
+            }
+            
             return response()->json(1);
         }else
         {
