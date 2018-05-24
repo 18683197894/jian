4b99508a8f3b7ea1cfb46b0f05b1b294f90505aa
zhenglunsen <1214422067@qq.com>
2018-05-24 09:48:16 +0800

    20180524-cnzz-https_edit

--- resources/views/Newpro/Home/public.blade.php
+++ resources/views/Newpro/Home/public.blade.php
@@ -149,6 +149,32 @@
 </body>
 @yield('js')
 <script src="{{ asset('/new/home/public/header.js') }}"></script>
-<script src="{{ asset('/new/home/public/wes.js') }}"></script>
+<!-- <script src="{{ asset('/new/home/public/wes.js') }}"></script> -->
+<script>
+    window.onload = function(){
+        wes();
+    }
+    function wes()
+    {
+        var src = $('#cnzz_stat_icon_1273388296 > a > img').attr('src');
+        var rpl = src.substr(0,5);
+        if(rpl == 'https') return false;
+        src = src.replace('http','https');
+        $('#cnzz_stat_icon_1273388296 > a > img').attr('src',src);
+    }
+
+    (function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+</script>
 
 </html>
