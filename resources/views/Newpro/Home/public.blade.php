b889b37a0ff6998e95efbf508554c124cdeafbf6
zhenglunsen <1214422067@qq.com>
2018-05-24 10:18:41 +0800

    20180524-cnzz-https_edit03

--- resources/views/Newpro/Home/public.blade.php
+++ resources/views/Newpro/Home/public.blade.php
@@ -143,12 +143,12 @@
 <!--版权-->
 <div class="copyright">
 <span>CopyRight 2017-2020 建商联盟版权所有 ICP备案：</span><i>蜀ICP备17010220 </i>&nbsp;&nbsp;
-<script type="text/javascript"> var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1273388296'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s13.cnzz.com/z_stat.php%3Fid%3D1273388296%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
+<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " https://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1273388296'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s13.cnzz.com/z_stat.php%3Fid%3D1273388296' type='text/javascript'%3E%3C/script%3E"));</script>
+
 </div>
 </body>
 @yield('js')
 <script src="{{ asset('/new/home/public/header.js') }}"></script>
-<!-- <script src="{{ asset('/new/home/public/wes.js') }}"></script> -->
 <script>
 
     (function(){
